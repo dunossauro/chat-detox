f5a008a3e964bf9ac846c122570de0b76085ab03
dunossauro <mendesxeduardo@gmail.com>
2025-04-26 06:47:41 -0300

    Adicionando frases das referências

--- organizador.py
+++ organizador.py
@@ -1,5 +1,19 @@
-with open('palavras.txt') as file:
-    lines = sorted(set(line.rstrip().lower() for line in file))
+from locale import LC_ALL, setlocale, strxfrm
+setlocale(LC_ALL, '')
 
-with open('palavras.txt', 'w') as file:
-    file.writelines(line + '\n' for line in lines)
+arquivos = ['palavras.txt', 'frases.txt']
+
+
+def formata(arquivo):
+    with open(arquivo) as file:
+        lines = sorted(
+            set(line.rstrip().lower() for line in file),
+            key=strxfrm
+        )
+
+    with open(arquivo, 'w') as file:
+        file.writelines(line + '\n' for line in lines)
+
+
+for arquivo in arquivos:
+    formata(arquivo)
